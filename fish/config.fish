bef26fb281d9e7f2c27b3b9de2b46f5415df4d90
abhishake1 <abhi9848038283@gmail.com>
2024-02-25 13:06:19 +0000

    .

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,9 @@ if status is-interactive
 end
 
 set fish_greeting ""
+export ANDROID_HOME="$HOME/Android/Sdk/"
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
+export PATH="$ANDROID_HOME/build-tools:$PATH"
+export PATH="/home/abhi/.local/bin:$PATH"
+
